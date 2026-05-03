9037377b712c79e4facaf47d34c0117222a2e0d4
Dimas Arya Arjuna <dimasaryarjuna06@gmail.com>
2026-05-03 17:53:26 +0700

    menambahkan dokumentasi

--- LOGIKA PEMROGRAMAN/PENDAHULUAN/latihan/Cetak Ganjil Genap.py
+++ LOGIKA PEMROGRAMAN/PENDAHULUAN/latihan/Cetak Ganjil Genap.py
@@ -3,9 +3,9 @@
 # proses: jika habis dibagi 2 = genap jika tidak ganjil
 # output: jenis angka
 
-angka = int(input('MASUKKAN ANGKA YANG INGIN DI PERIKSA: '))
+angka = int(input('MASUKKAN ANGKA YANG INGIN DI PERIKSA: ')) #input
 
-if angka % 2 == 0:
-    print("ANGKA GENAP")
+if angka % 2 == 0: #proses
+    print("ANGKA GENAP") #output
 else:
-    print("ANGKA GANJIL")
+    print("ANGKA GANJIL") #output
