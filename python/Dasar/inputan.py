5c648a488ec056846d08e8d07e34e3dcd1de35d3
Dimas Arya Arjuna <dimasaryarjuna06@gmail.com>
2026-07-22 16:01:55 +0700

    menambahkan contoh penerapan inputan

---
+++ python/Dasar/inputan.py
@@ -0,0 +1,21 @@
+# untuk memasukkan data dari pengguna, kita dapat menggunakan fungsi input() yang disediakan oleh Python. Fungsi ini akan menunggu pengguna untuk memasukkan data melalui keyboard, dan data yang dimasukkan akan dikembalikan sebagai string. Berikut adalah contoh penggunaan fungsi input() untuk meminta pengguna memasukkan nama dan umur mereka:
+
+# meminta pengguna memasukkan nama
+nama = input("Masukkan nama Anda: ")
+# data yang di ketikkan oleh pengguna akan disimpan dalam variable (kotak) nama 
+
+# meminta pengguna memasukkan umur
+umur = input("Masukkan umur Anda: ")
+# data yang di ketikkan oleh pengguna akan disimpan dalam variable (kotak) umur
+
+# menampilkan data yang dimasukkan oleh pengguna
+print("Nama Anda adalah:", nama)
+print("Umur Anda adalah:", umur)
+
+'''
+Masukkan nama Anda: Dimas 
+Masukkan umur Anda: 21
+Nama Anda adalah: Dimas
+Umur Anda adalah: 21
+
+'''
