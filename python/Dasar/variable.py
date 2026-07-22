b54f852b809b814555cae0bb5c9292c7fa695b6c
Dimas Arya Arjuna <dimasaryarjuna06@gmail.com>
2026-07-22 15:46:48 +0700

    menampilkan hasil

--- python/Dasar/variable.py
+++ python/Dasar/variable.py
@@ -13,3 +13,16 @@ gaji = 5000.00
 # variable status = menyimpan data status apakah bernilai benar atau salah
 menikah = False
 
+# CONTOH MENAMPILKAN / MENGAMBIL DATA DARI KOTAK (variable)
+# UNTUK MENAMPILKAN DATA DARI KOTAK (variable) KITA BISA MENGGUNAKAN FUNGSI print()
+print("Nama:", nama)
+print("Umur:", umur)
+print("Gaji:", gaji)
+print("Status Menikah:", menikah)
+
+'''
+Nama: John Doe
+Umur: 25
+Gaji: 5000.0
+Status Menikah: False
+'''
