ca9cf4466d9ca6e540c0e2560ec5bd2467729e9d
Dimas Arya Arjuna <dimasaryarjuna06@gmail.com>
2026-07-22 15:55:23 +0700

    menambahkan file yang membahas tipe data primitif

---
+++ python/Dasar/tipe_data.py
@@ -0,0 +1,33 @@
+# Tipe Data = Bentuk data yang digunakan untuk menyimpan informasi dalam program.
+
+# string Tipe data string digunakan untuk menyimpan teks atau karakter.
+# ditandai dengan penulisan data yang diapit oleh tanda kutip tunggal (' ') atau tanda kutip ganda (" ").
+    # tidak ada perbedaan antara tanda kutip tunggal dan tanda kutip ganda, keduanya dapat digunakan secara bergantian.
+# contoh
+nama = "John Doe"
+nama2 = 'Jane Smith'
+
+# int Tipe data integer digunakan untuk menyimpan bilangan bulat (tanpa desimal).
+# contoh
+umur = 25
+
+# float Tipe data float digunakan untuk menyimpan bilangan desimal.
+# contoh
+gaji = 5000.00
+
+# boolean Tipe data boolean digunakan untuk menyimpan nilai benar (True) atau salah (False).
+# contoh
+menikah = False
+
+
+print("Nama dengan tipe data string:", type(nama))
+print("Umur dengan tipe data integer:", type(umur))
+print("Gaji dengan tipe data float:", type(gaji))
+print("Status Menikah dengan tipe data boolean:", type(menikah))
+
+'''
+Nama dengan tipe data string: <class 'str'>
+Umur dengan tipe data integer: <class 'int'>
+Gaji dengan tipe data float: <class 'float'>
+Status Menikah dengan tipe data boolean: <class 'bool'>
+'''
